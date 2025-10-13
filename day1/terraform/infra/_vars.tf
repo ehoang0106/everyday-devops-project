c9ba0939ac7b27ace4b6006269dfef772ab318d0
Khoa Hoang <khoahoang1698@gmail.com>
2025-10-13 09:27:10 -0700

    update

--- day1/terraform/infra/_vars.tf
+++ day1/terraform/infra/_vars.tf
@@ -1,7 +1,7 @@
 #ec2 ami
 
 variable "ami" {
-  description = "Ubuntu 24.04 LTS AMI ID"
+  description = "Amazon Linux 2023 AMI 2023.9.20250929.0 x86_64 HVM kernel-6.1"
   type        = string
-  default     = "ami-014e30c8a36252ae5"
+  default     = "ami-0b967c22fe917319b"
 }
