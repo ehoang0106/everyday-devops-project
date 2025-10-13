99e8d71d12c47c4d6fc362581e019ee5f6e0f414
Khoa Hoang <khoahoang1698@gmail.com>
2025-10-13 14:30:32 -0700

    update

--- day1/terraform/script.sh
+++ day1/terraform/script.sh
@@ -15,3 +15,9 @@ sudo dnf install -y java-1.8.0-amazon-corretto-devel
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64
 
 export PATH=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre/bin/:$PATH
+
+sudo dnf install git -y
+
+cd ~/
+git clone https://github.com/ehoang0106/network-web-project.git && cd network-web-project
+
