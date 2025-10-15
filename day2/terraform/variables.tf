d273c14a193e6e8b86876dda5d3443fbe920d795
Khoa Hoang <khoahoang1698@gmail.com>
2025-10-15 09:47:01 -0700

    variable

---
+++ day2/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type = string
+  default = "us-west-1"
+}
+
+variable "project" {
+  type = string
+  default = "vpc-peering-infra"
+}
+
+variable "env_name" {
+  type = string
+  default = "dev"
+}
