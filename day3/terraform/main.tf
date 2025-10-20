fe7f97ac6602ad6b86be069630f4b2a9781fda02
Khoa Hoang <khoahoang1698@gmail.com>
2025-10-20 08:35:18 -0700

    route 53 record for api gateway custom domain

--- day3/terraform/main.tf
+++ day3/terraform/main.tf
@@ -180,14 +180,15 @@ resource "aws_apigatewayv2_api_mapping" "custom_domain_mapping" {
 
 #route 53 record to point to the api gateway custom domain
 
-# resource "aws_route53_record" "api_gateway_record"{
-#   zone_id = "Z09372142GLGU75DMF3RP"
-#   name = "count.khoah.net"
-#   type = "A"
-#   alias {
-#     name                   = aws_apigatewayv2_domain_name.custom_domain.domain_name
-#     zone_id                = aws_apigatewayv2_domain_name.custom_domain.hosted_zone_id
-#     evaluate_target_health = true
-#   }
-# }
+resource "aws_route53_record" "api_gateway_record"{
+  zone_id = "Z09372142GLGU75DMF3RP"
+  name = "count.khoah.net"
+  type = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.custom_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.custom_domain.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
 
