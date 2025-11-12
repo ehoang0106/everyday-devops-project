4f2526087dd6904b5aed849214268fbed2dad4c3
Khoa Hoang <khoahoang1698@gmail.com>
2025-11-12 15:09:09 -0800

    change filebase to base64encoded

--- day7/terraform/infra/launch_template.tf
+++ day7/terraform/infra/launch_template.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "weather_app_launch_template" {
   name = "weather-app-launch-template"
   image_id = "ami-01eb4eefd88522422"
-  #instance_type = "t3.small"
+  instance_type = "t3.medium"
 
   network_interfaces {
     associate_public_ip_address = true
@@ -13,9 +13,7 @@ resource "aws_launch_template" "weather_app_launch_template" {
   }
 
   # read the local user_data.sh and supply it to the launch template (base64-encoded)
-  user_data = filebase64("${path.module}/user_data.sh")
-
-  security_group_names = [data.aws_security_group.weather_sg.id ]
+  user_data = base64encode(file("${path.module}/user_data.sh"))
 
   tags = {
     Name = "weather-app-launch-template"
